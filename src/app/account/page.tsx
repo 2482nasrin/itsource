502ec579b9f24eaab34dbeb082002fddc14435aa
unknown <2482nasrin>
2026-08-09 13:38:28 +0600

    fix empty account page

---
+++ src/app/account/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AccountPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Account Page</h1>
+    </div>
+  );
+}
